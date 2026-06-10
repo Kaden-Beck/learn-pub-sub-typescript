57fb91455d5075f67363c9939e56cc6ba49e55dc
Kaden-Beck <kadenb12@gmail.com>
2026-06-09 22:15:13 -0600

    Enhance Peril client initialization with queue declaration and binding

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,18 +1,26 @@
 import amqp from 'amqplib';
 import { clientWelcome } from '../internal/gamelogic/gamelogic.js';
+import { SimpleQueueType, declareAndBind } from '../internal/pubsub/bind.js';
+import { ExchangePerilDirect, PauseKey } from '../internal/routing/routing.js';
 
 async function main() {
   const rabbitConnString = 'amqp://guest:guest@localhost:5672/';
   const conn = await amqp.connect(rabbitConnString);
 
-  console.log("Starting Peril client...");
+  console.log('Starting Peril client...');
 
   const username = await clientWelcome();
 
-
+  const [channel, assertQueue] = await declareAndBind(
+    conn,
+    ExchangePerilDirect,
+    `${PauseKey}.${username}`,
+    PauseKey,
+    SimpleQueueType.Transient,
+  );
 }
 
 main().catch((err) => {
-  console.error("Fatal error:", err);
+  console.error('Fatal error:', err);
   process.exit(1);
 });
