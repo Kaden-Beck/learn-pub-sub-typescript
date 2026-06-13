beae4523eca88a8a8d69023d60849e5f4d62587d
Kaden-Beck <kadenb12@gmail.com>
2026-06-13 17:30:28 -0600

    Add input handling for pause, resume, and quit commands in the server

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,7 @@ import amqp, { type ConfirmChannel } from 'amqplib';
 import { publishJSON } from '../internal/pubsub/publish.js';
 import { ExchangePerilDirect, PauseKey } from '../internal/routing/routing.js';
 import type { PlayingState } from '../internal/gamelogic/gamestate.js';
+import { getInput, printServerHelp } from '../internal/gamelogic/gamelogic.js';
 
 async function main() {
   const rabbitConnString = 'amqp://guest:guest@localhost:5672/';
@@ -13,6 +14,35 @@ async function main() {
   await publishJSON(confirm, ExchangePerilDirect, PauseKey, {
     isPaused: true,
   } as PlayingState);
+
+  printServerHelp();
+
+  while (true) {
+    const words = await getInput();
+
+    if (words.length !== 0) {
+      const firstWord = words[0];
+      if (firstWord === 'pause') {
+        console.log('Sending a pause message... ');
+
+        await publishJSON(confirm, ExchangePerilDirect, PauseKey, {
+          isPaused: true,
+        } as PlayingState);
+      } else if (firstWord === 'resume') {
+        console.log('Sending a resume message... ');
+
+        await publishJSON(confirm, ExchangePerilDirect, PauseKey, {
+          isPaused: false,
+        } as PlayingState);
+      } else if (firstWord === 'quit') {
+        console.log('Exiting...');
+        break;
+      } else {
+        console.log('Invalid input... Try again.');
+        continue;
+      }
+    }
+  }
 
   ['SIGINT', 'SIGTERM'].forEach((signal) =>
     process.on(signal, async () => {
